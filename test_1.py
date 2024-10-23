9c2483d7ce078d3fdbfe48efb9bb057050a355cd
Hustle <real4meal@gmail.com>
2024-10-24 02:34:40 +0500

    Added fixture and faker

--- test_1.py
+++ test_1.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from faker import Faker
 
 
+fake = Faker()
 LOGIN_BUTTON = (By.XPATH, "//a[@class='global_action_link']")
 EMAIL_INPUT = (By.XPATH, '//input[@type="text"]')
 PASSWORD_INPUT = (By.XPATH, '//input[@type="password"]')
@@ -13,16 +15,16 @@ EXPECTED_ERROR_TEXT = "Пожалуйста, проверьте свой пар�
 TIMEOUT = 10
 URL = 'https://store.steampowered.com'
 
-def test_login():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+def test_login(driver):
     wait = WebDriverWait(driver, TIMEOUT)
     driver.get(f'{URL}')
 
+    email = fake.email()
+    password = fake.password(length=12, special_chars=True, digits=True, upper_case=True, lower_case=True)
 
     wait.until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
-    wait.until(EC.visibility_of_element_located(EMAIL_INPUT)).send_keys('mail@mail.com')
-    wait.until(EC.visibility_of_element_located(PASSWORD_INPUT)).send_keys('password')
+    wait.until(EC.visibility_of_element_located(EMAIL_INPUT)).send_keys(email)
+    wait.until(EC.visibility_of_element_located(PASSWORD_INPUT)).send_keys(password)
     wait.until(EC.element_to_be_clickable(SUBMIT_BUTTON)).click()
 
     actual_error_text = wait.until(EC.visibility_of_element_located(ERROR_MESSAGE)).text
